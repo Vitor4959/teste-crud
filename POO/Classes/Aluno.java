7459dca9fbe99038f8be312be9e1db23afc6bfbd
Vitor4959 <vitorpedrpgabriel@gmail.com>
2025-09-11 21:43:27 -0300

    POO

---
+++ POO/Classes/Aluno.java
@@ -0,0 +1,12 @@
+package Classes;
+
+public class Aluno {
+
+    private String Prontuario;
+    private String nome;
+
+    public void estudar() {
+        System.out.println("estudando...");
+    }
+
+}
