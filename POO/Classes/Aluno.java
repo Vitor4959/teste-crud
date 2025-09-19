a6838a663d4bc76f36e1e443740bc72e8859f6d4
Vitor4959 <vitorpedrogabriel@gmail.com>
2025-09-18 22:00:06 -0300

    SLA

--- POO/Classes/Aluno.java
+++ POO/Classes/Aluno.java
@@ -2,11 +2,27 @@ package Classes;
 
 public class Aluno {
 
-    private String Prontuario;
+    private int Prontuario;
     private String nome;
 
     public void estudar() {
         System.out.println("estudando...");
+    }
+
+    public int getProntuario() {
+        return Prontuario;
+    }
+
+    public void setProntuario(int Prontuario) {
+        this.Prontuario = Prontuario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
 }
